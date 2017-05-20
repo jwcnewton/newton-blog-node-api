45112aa6bf235c943673783e29044cbed13aedf0
jwcnewton <jwcnewton@gmail.com>
2017-05-20 22:17:30 +0100

    Added access control to fix cors issue

--- index.js
+++ index.js
@@ -8,11 +8,32 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 mongo.connectWithRetry().then(function () {
     const blogs = require('./controllers/postController')(app);
 
     const server = app.listen(process.env.PORT || 8080, function () {
-        console.log("Listening on 3000");
+        let port = server.address().port;
+        console.log("Listening on " + port);
     });
 });
 
